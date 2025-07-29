b996a22078d65c8311230f4566b37c5f1920c9c2
Harsh Pratap <133808138+harshkunz@users.noreply.github.com>
2025-07-29 23:05:34 +0530

    add: q

--- #.cpp
+++ #.cpp
@@ -1,1 +1,28 @@
+2411 
+  
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> bits(31, -1);
+        vector<int> r(n, 0);
 
+        for(int i=n-1; i>=0; i--){
+            
+            for(int j=0; j<31; j++){
+                if((nums[i] >> j) & 1){
+                    bits[j] = i;
+                }
+            }
+
+            int x = i;
+            for(int j=0; j<31; j++){
+                x = max(x, bits[j]);
+            }
+
+            r[i] = x - i + 1;
+        }
+
+        return r;
+    }
+};
